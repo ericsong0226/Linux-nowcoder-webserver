b246f11bd967ecd8dd13dd2c1265c41cbed5b2d2
ericsong <ericsong0226@gmail.com>
2023-01-24 16:15:36 +0800

    add tcp_client review

--- review/tcp_client.c
+++ review/tcp_client.c
@@ -1,4 +1,10 @@
-// TCP 通信的客户端
+/* ************************************************************************
+> File Name:     tcp_client.c
+> Author:        程序员Eric
+> 微信公众号:    源水本木
+> Created Time:  Tue 24 Jan 2023 03:50:23 PM CST
+> Description:   
+ ************************************************************************/
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
@@ -7,14 +13,13 @@
 
 int main() {
 
-    // 1.创建套接字
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
+   
         perror("socket");
         exit(-1);
     }
 
-    // 2.连接服务器端
     struct sockaddr_in serveraddr;
     serveraddr.sin_family = AF_INET;
     inet_pton(AF_INET, "192.168.179.130", &serveraddr.sin_addr.s_addr);
@@ -22,15 +27,16 @@ int main() {
     int ret = connect(fd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
 
     if (ret == -1) {
+   
         perror("connect");
         exit(-1);
     }
 
     char recvBuf[1024] = {0};
-    // 3.通信
-    while (1) {
+
+    while(1) {
+   
         char * data = "hello, i am client";
-        // 给服务器端发送数据
         write(fd, data, strlen(data));
 
         sleep(1);
@@ -38,18 +44,19 @@ int main() {
         memset(recvBuf, 0, 1024);
         int len = read(fd, recvBuf, sizeof(recvBuf));
         if (len == -1) {
+       
             perror("read");
             exit(-1);
         } else if (len > 0) {
-            printf("recv server data :%s\n", recvBuf);
+       
+            printf("recv server data : %s\n", recvBuf);
         } else if (len == 0) {
-            // 表示服务器端断开连接
+       
             printf("server closed...\n");
             break;
         }
     }
 
-    // 关闭连接
     close(fd);
 
     return 0;
