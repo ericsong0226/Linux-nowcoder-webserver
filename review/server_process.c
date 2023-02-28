4b502b2fc0ded72f2f8c2e10561595cc612e934d
ericsong <ericsong0226@gmail.com>
2023-02-28 21:17:07 +0800

    modified server_process review

--- review/server_process.c
+++ review/server_process.c
@@ -6,3 +6,46 @@
 > Description:   
  ************************************************************************/
 #include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <wait.h>
+#include <errno.h>
+
+void recyleChild(int arg) {
+
+    while (1) {
+   
+        int ret = waitpid(-1, NULL, WHOHANG);
+        if (ret == -1) {
+       
+            break;
+        } else if (ret == 0) {
+       
+            break;
+        } else if (ret > 0) {
+       
+            printf("child %d be recyle", ret);
+        }
+    }
+}
+
+int main() {
+
+    struct sigaction act;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+    act.sa_handler = recyleChild;
+    sigaction(SIGCHILD, &act, NULL);
+
+    int lfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (lfd == -1) {
+   
+        perror("socket");
+        exit(-1);
+    }
+
+    struct sockaddr_in saddr;
+}
