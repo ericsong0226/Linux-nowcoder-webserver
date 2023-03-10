ad137f22f93bb412ca9934df8fe47cb19b1caf60
ericsong <ericsong0226@gmail.com>
2023-03-10 09:21:11 +0800

    add poll review

---
+++ review/poll.c
@@ -0,0 +1,18 @@
+/* ************************************************************************
+> File Name:     poll.c
+> Author:        程序员Eric
+> 微信公众号:    源水本木
+> Created Time:  Fri 10 Mar 2023 09:19:44 AM CST
+> Description:   
+ ************************************************************************/
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <poll.h>
+
+int main() {
+
+
+}
