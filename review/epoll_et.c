76ed7b5d8e5d22dab93f317f56e6e8a5a081b4ee
ericsong <ericsong0226@gmail.com>
2023-03-14 09:44:10 +0800

    add epoll_et review

--- review/epoll_et.c
+++ review/epoll_et.c
@@ -6,3 +6,21 @@
 > Description:   
  ************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main() {
+
+    int lfd = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in saddr;
+    saddr.sin_port = htons(9999);
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(lfd, (struct sockaddr *)&saddr, sizeof(saddr));
+}
