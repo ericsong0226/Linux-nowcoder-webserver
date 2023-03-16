b05cd0b33a15d78b627b478340c448be01069eb1
ericsong <ericsong0226@gmail.com>
2023-03-16 23:19:43 +0800

    add client_et review

---
+++ review/client_et.c
@@ -0,0 +1,17 @@
+/* ************************************************************************
+> File Name:     client_et.c
+> Author:        程序员Eric
+> 微信公众号:    源水本木
+> Created Time:  Thu 16 Mar 2023 09:25:07 AM CST
+> Description:   
+ ************************************************************************/
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+
+    return 0;
+}
