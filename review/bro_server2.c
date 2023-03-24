23dd2ce0bd20d7296bb7412fc04d298b61e0d3f4
ericsong <ericsong0226@gmail.com>
2023-03-24 09:16:29 +0800

    create bro_server2 review

---
+++ review/bro_server2.c
@@ -0,0 +1,12 @@
+/* ************************************************************************
+> File Name:     bro_server2.c
+> Author:        程序员Eric
+> 微信公众号:    源水本木
+> Created Time:  Fri 24 Mar 2023 09:15:17 AM CST
+> Description:   
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
