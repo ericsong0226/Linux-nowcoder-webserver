a3d249a3bd26517b0d2e1d5fec52eae091447bab
ericsong <ericsong0226@gmail.com>
2023-03-25 21:18:43 +0800

    add bro-server2 review

--- review/bro_server2.c
+++ review/bro_server2.c
@@ -10,3 +10,37 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+
+int main() {
+
+    int fd = socket(PF_INET, SOCK_DGRAM, 0);
+    if (fd == -1) {
+   
+        perror("socket");
+        exit(-1);
+    }
+
+    int op = 1;
+    setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &op, sizeof(op));
+
+    struct sockaddr_in cliaddr;
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_port = htons(9999);
+    inet_pton(AF_INET, "192.168.179.130", &cliaddr.sin_addr.s_addr);
+
+    int num = 0;
+    while (1) {
+   
+        char sendBuf[128];
+
+        sprintf(sendBuf, "hello, client...%d\n", num++);
+
+        sendto(fd, sendBuf, strlen(sendBuf) + 1, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+        printf("bro data : %s\n", sendBuf);
+        sleep(1);
+    }
+
+    close(fd);
+
+    return 0;
+}
