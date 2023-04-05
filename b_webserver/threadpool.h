c8e5a2e11e5103d87c15650a62821bb9fbce39c7
ericsong <ericsong0226@gmail.com>
2023-04-05 14:31:20 +0800

    create threadpool construct func

--- b_webserver/threadpool.h
+++ b_webserver/threadpool.h
@@ -40,4 +40,39 @@ private:
     sem m_queuestat;
 
     bool m_stop;
+};
+
+template<typename T>
+threadpool<T>::threadpool(int thread_number, int max_requests) : 
+    m_thread_number(thread_number), m_max_requests(max_requests),
+    m_stop(false), m_threads(NULL) {
+        
+        if ((thread_number <= 0) || (max_requests <= 0)) {
+            throw std::exception();
+        }
+
+        m_threads = new pthread_t[m_thread_number];
+        if (!m_threads) {
+            throw std::exception();
+        }
+
+        for (int i = 0; i < thread_number; ++i) {
+            printf("create the %dth thread\n", i);
+
+            if (pthread_create(m_threads + i, NULL, worker, this) != 0) {
+                delete[] m_threads;
+                throw std::exception();
+            }
+
+            if (pthread_detach(m_threads[i])) {
+                delete[] m_threads;
+                throw std::exception();
+            }
+        }
+    } 
+
+template<typename T>
+threadpool<T>::~threadpool() {
+    delete[] m_threads;
+    m_stop = true;
 }
