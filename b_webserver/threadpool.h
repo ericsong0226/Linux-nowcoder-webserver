a25fdd91ac51e9da1230f645f8707d1f58a6cd37
ericsong <ericsong0226@gmail.com>
2023-04-05 14:07:00 +0800

    create threadpool

--- b_webserver/threadpool.h
+++ b_webserver/threadpool.h
@@ -2,7 +2,42 @@
 > File Name:     threadpool.h
 > Author:        程序员Eric
 > 微信公众号:    源水本木
-> Created Time:  Sun 02 Apr 2023 10:11:34 PM CST
+> Created Time:  Wed 05 Apr 2023 01:57:25 PM CST
 > Description:   
  ************************************************************************/
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
 
+#include <pthread.h>
+#include <list>
+#include <exception>
+#include <cstdio>
+#include "locker.h"
+
+template<typename T>
+clase threadpool {
+public:
+    threadpool(int thread_number = 8, int max_requests = 10000);
+    ~threadpool();
+    bool append(T* request);
+
+private:
+    static void* worker(void * arg);
+    void run();
+
+private:
+    // thread number
+    int m_thread_number;
+
+    pthread_t * m_threads;
+
+    int m_max_requests;
+
+    std::list<T*> m_workqueue;
+
+    locker m_queuelocker;
+    
+    sem m_queuestat;
+
+    bool m_stop;
+}
