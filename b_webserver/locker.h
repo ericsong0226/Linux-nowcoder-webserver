1b22f08f175cd06c1e45b058575a71182aad0b0a
ericsong <ericsong0226@gmail.com>
2023-04-09 19:38:55 +0800

    add locker

---
+++ b_webserver/locker.h
@@ -0,0 +1,109 @@
+/* ************************************************************************
+> File Name:     locker.h
+> Author:        程序员Eric
+> 微信公众号:    源水本木
+> Created Time:  Sun 09 Apr 2023 06:26:58 PM CST
+> Description:   
+ ************************************************************************/
+#ifndef LOCKER_H
+#define LOCKER_H
+
+#include <pthread.h>
+#include <execption>
+#include <semaphore.h>
+
+class locker {
+public:
+    locker() {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0) {
+            throw std::execption();
+        }
+    }
+
+    ~locker() {
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    bool lock() {
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+
+    bool unlock() {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+
+    pthread_mutex_t * get() {
+        return &m_mutex;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+
+class cond {
+public:
+    cond() {
+        if (pthread_cond_init(&m_cond, NULL) != 0) {
+            throw std::execption();
+        }
+    }
+
+    ~cond() {
+        pthread_cond_destroy(&m_cond);
+    }
+
+    bool wait(pthread_mutex_t * m_mutex) {
+        int ret = 0;
+        ret = pthread_cond_wait(&m_cond, m_mutex) == 0;
+        return ret == 0;
+    }
+
+    bool timewait(pthread_mutex_t * m_mutex, struct timespec t) {
+        int ret = 0;
+        ret = pthread_cond_timedwait(&m_cond, m_mutex, &t);
+        return ret == 0;
+    }
+
+    bool signal() {
+        return pthread_cond_signal(&m_cond) == 0;
+    }
+
+    bool broadcast() {
+        return pthread_cond_broadcast(&m_cond) == 0;
+    }
+
+private:
+    pthread_cond_t m_cond;
+};
+
+class sem {
+public:
+    sem() {
+        if (sem_init(&m_sem, 0, 0) != 0) {
+            throw std::execption();
+        }
+    }
+
+    sem(int num) {
+        if (sem_init(&m_sem, 0, num) != 0) {
+            throw std::execption();
+        }
+    }
+
+    ~sem() {
+        sem_destroy(&m_sem);
+    }
+
+    bool wait() {
+        return sem_wait(&m_sem) == 0;
+    }
+
+    bool post() {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+
+#endif
