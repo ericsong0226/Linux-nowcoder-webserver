76f248ec505f3a3638ff009da5098112d73920cc
ericsong <ericsong0226@gmail.com>
2023-04-10 21:20:28 +0800

    add main

--- b_webserver/main.cpp
+++ b_webserver/main.cpp
@@ -8,3 +8,51 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include "locker.h"
+#include "threadpool.h"
+#include "http_conn.h"
+
+#define MAX_FD 65536
+#define MAX_EVENT_NUMBER 10000
+
+void addsig(int sig, void(handler)(int)) {
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = handler;
+    sigfillset (&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        printf("usage : %s port_number\n", basename(argv[0]));
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+    addsig(SIGPIPE, SIG_IGN);
+
+    threadpool<http_conn>* pool = NULL;
+    try {
+        pool = new threadpool<http_conn>;
+    } catch(...) {
+        return 1;
+    }
+
+    http_conn* users = new http_conn[MAX_FD];
+
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+
+    int ret = 0;
+    struct sockaddr_in address;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+}
