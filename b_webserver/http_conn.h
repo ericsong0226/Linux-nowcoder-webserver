76586d4c2158ce8a41643bd70b7d66189bf9c451
ericsong <ericsong0226@gmail.com>
2023-04-13 09:14:12 +0800

    create http_conn

---
+++ b_webserver/http_conn.h
@@ -0,0 +1,9 @@
+/* ************************************************************************
+> File Name:     http_conn.h
+> Author:        程序员Eric
+> 微信公众号:    源水本木
+> Created Time:  Thu 13 Apr 2023 09:13:11 AM CST
+> Description:   
+ ************************************************************************/
+#ifndef HTTPCONNECTION_H
+#define HTTPCONNECTION_H
