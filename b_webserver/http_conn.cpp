dbaac9a9c4613b5bedf7806db3ad427fea4245b1
ericsong <ericsong0226@gmail.com>
2023-05-15 21:13:36 +0800

    add error_500_form

--- b_webserver/http_conn.cpp
+++ b_webserver/http_conn.cpp
@@ -10,6 +10,12 @@
 const char* ok_200_title = "OK";
 const char* error_400_title = "Bad Request";
 const char* error_400_form = "Your request has bad syntax or is inherently impossible to satisfy.\n";
+const char* error_403_title = "Forbidden";
+const char* error_403_form = "You do not have permission to get file from this server.\n";
+const char* error_404_title = "Not Found";
+const char* error_404_form = "The requested file was not found on this server.\n";
+const char* error_500_title = "Internal Error";
+const char* error_500_form = "There was an unusual problem serving the requested file.\n";
 
 const char* doc_root = "/home/song/linux/webserver/resources/";
 
